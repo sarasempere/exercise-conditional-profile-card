86c799ce641c8040fc855edf7373854e59167b74
sarasempere <semperesara@gmail.com>
2020-07-01 19:38:11 +0000

    ejercicios

--- src/js/index.js
+++ src/js/index.js
@@ -33,7 +33,9 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>${variables.name != null ? variables.name : "add name"} ${
+    variables.name != null ? variables.lastname : "add lastname"
+  } </h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
